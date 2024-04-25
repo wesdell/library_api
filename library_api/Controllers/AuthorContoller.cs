13f976f9287af9685158f61c5ede6bf7b08558c1
Wesdell <wesdell17@gmail.com>
2024-04-24 21:18:06 -0500

    New endpoint to get an author.

---
+++ library_api/Controllers/AuthorContoller.cs
@@ -0,0 +1,17 @@
+﻿using library_api.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace library_api.Controllers
+{
+	[ApiController]
+	[Route("api/authors")]
+	public class AuthorContoller : ControllerBase
+	{
+		[HttpGet]
+		public ActionResult<List<Author>> Get()
+		{
+			return Ok(new List<Author>() { new Author() { Id = 0, Name = "Jane Doe" } });
+		}
+
+	}
+}

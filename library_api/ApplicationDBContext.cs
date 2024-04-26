5213e519727572cc32543763cb129df591d24548
Wesdell <wesdell17@gmail.com>
2024-04-25 21:40:09 -0500

    Define AppDBContext file.

---
+++ library_api/ApplicationDBContext.cs
@@ -0,0 +1,15 @@
+﻿using library_api.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace library_api
+{
+	public class ApplicationDBContext : DbContext
+	{
+		public ApplicationDBContext(DbContextOptions options) : base(options)
+		{
+
+		}
+
+		public DbSet<Author> Author { get; set; }
+	}
+}

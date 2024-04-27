0cc6db51923885476821599ab902083f355e53e7
Wesdell <wesdell17@gmail.com>
2024-04-26 21:51:15 -0500

    New endpoints to get a book by id and to create a book.

---
+++ library_api/Controllers/BookController.cs
@@ -0,0 +1,44 @@
+﻿using library_api.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace library_api.Controllers
+{
+	[ApiController]
+	[Route("api/books")]
+	public class BookController : ControllerBase
+	{
+		private readonly ApplicationDBContext _context;
+
+		public BookController(ApplicationDBContext context)
+		{
+			this._context = context;
+		}
+
+		[HttpGet("{id:int}")]
+		public async Task<ActionResult<Book>> GetById(int id)
+		{
+			bool bookExists = await this._context.Book.AnyAsync(book => book.Id == id);
+			if (!bookExists)
+			{
+				return NotFound();
+			}
+
+			return await this._context.Book.Include(book => book.Author).FirstOrDefaultAsync(book => book.Id == id);
+		}
+
+		[HttpPost]
+		public async Task<ActionResult> Post(Book book)
+		{
+			bool authorExists = await this._context.Author.AnyAsync(author => author.Id == book.AuthorId);
+			if (!authorExists)
+			{
+				return BadRequest($"Author: {book.AuthorId}, does not exist.");
+			}
+
+			this._context.Add(book);
+			await this._context.SaveChangesAsync();
+			return Ok();
+		}
+	}
+}

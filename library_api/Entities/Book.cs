ae6a797d0cd0179c07eec33b44e268185a873004
Wesdell <wesdell17@gmail.com>
2024-04-26 21:15:07 -0500

    Add Book entity.

---
+++ library_api/Entities/Book.cs
@@ -0,0 +1,12 @@
+﻿using library_api.Interfaces;
+
+namespace library_api.Entities
+{
+	public class Book : IBook
+	{
+		public int Id { get; set; }
+		public int AuthorId { get; set; }
+		public string Title { get; set; }
+		public IAuthor Author { get; set; }
+	}
+}

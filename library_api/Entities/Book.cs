09b6831e1823c32847b5545f90a250083a58821f
Wesdell <wesdell17@gmail.com>
2024-05-04 20:55:47 -0500

    Set a validation for Title field on Book entity.

--- library_api/Entities/Book.cs
+++ library_api/Entities/Book.cs
@@ -1,11 +1,13 @@
 ﻿using library_api.Interfaces;
 using library_api.Validations;
+using System.ComponentModel.DataAnnotations;
 
 namespace library_api.Entities
 {
 	public class Book : IBook
 	{
 		public int Id { get; set; }
+		[StringLength(maximumLength: 250)]
 		[FirstLetterCapitalized]
 		public string Title { get; set; }
 	}

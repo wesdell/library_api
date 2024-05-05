7643248cb64eed25dd16be0a8a00b339b3ce5aa0
Wesdell <wesdell17@gmail.com>
2024-05-04 21:40:02 -0500

    New DTO for expose necessary Author fields.

---
+++ library_api/DTOs/CreateAuthorDTO.cs
@@ -0,0 +1,13 @@
+﻿using library_api.Validations;
+using System.ComponentModel.DataAnnotations;
+
+namespace library_api.DTOs
+{
+	public class CreateAuthorDTO
+	{
+		[Required]
+		[StringLength(maximumLength: 150)]
+		[FirstLetterCapitalized]
+		public string Name { get; set; }
+	}
+}

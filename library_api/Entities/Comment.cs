27f6f8b8b557d4dee283b22bddf5054a94c74f0a
Wesdell <wesdell17@gmail.com>
2024-05-06 16:21:10 -0500

    Add Comment Entity.

---
+++ library_api/Entities/Comment.cs
@@ -0,0 +1,12 @@
+﻿using library_api.Interfaces;
+
+namespace library_api.Entities
+{
+	public class Comment : IComment
+	{
+		public int Id { get; set; }
+		public string Content { get; set; }
+		public int BookId { get; set; }
+		public Book Book { get; set; }
+	}
+}

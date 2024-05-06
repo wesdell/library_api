4082b28db2f26630dae39d9ca8f74a9a3b60cee2
Wesdell <wesdell17@gmail.com>
2024-05-06 16:19:53 -0500

    New types for Book interface.

--- library_api/Interfaces/Book.cs
+++ library_api/Interfaces/Book.cs
@@ -1,8 +1,11 @@
-﻿namespace library_api.Interfaces
+﻿using library_api.Entities;
+
+namespace library_api.Interfaces
 {
 	public interface IBook
 	{
 		int Id { get; set; }
 		string Title { get; set; }
+		List<Comment> Comments { get; set; }
 	}
 }

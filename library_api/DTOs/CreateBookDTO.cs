b00d95f64b5eebf720d31baccb8c47a2f5ee5ffd
Wesdell <wesdell17@gmail.com>
2024-05-05 21:37:00 -0500

    New CreateBookDTO.

---
+++ library_api/DTOs/CreateBookDTO.cs
@@ -0,0 +1,12 @@
+﻿using library_api.Validations;
+using System.ComponentModel.DataAnnotations;
+
+namespace library_api.DTOs
+{
+	public class CreateBookDTO
+	{
+		[StringLength(maximumLength: 250)]
+		[FirstLetterCapitalized]
+		public string Title { get; set; }
+	}
+}

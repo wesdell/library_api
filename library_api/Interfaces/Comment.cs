19bc3a89316f75dd19e626dc0d9fb4407c3b4972
Wesdell <wesdell17@gmail.com>
2024-05-06 16:20:13 -0500

    Add new types for Comment interface.

---
+++ library_api/Interfaces/Comment.cs
@@ -0,0 +1,12 @@
+﻿using library_api.Entities;
+
+namespace library_api.Interfaces
+{
+	public interface IComment
+	{
+		int Id { get; set; }
+		string Content { get; set; }
+		int BookId { get; set; }
+		Book Book { get; set; }
+	}
+}

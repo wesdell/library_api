76af26eddee7bb191370552f7fcdb342f488159b
Wesdell <wesdell17@gmail.com>
2024-05-06 17:20:38 -0500

    Add AuthorBook table.

--- library_api/ApplicationDBContext.cs
+++ library_api/ApplicationDBContext.cs
@@ -7,8 +7,16 @@ namespace library_api
 	{
 		public ApplicationDBContext(DbContextOptions options) : base(options) { }
 
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			base.OnModelCreating(modelBuilder);
+
+			modelBuilder.Entity<AuthorBook>().HasKey(el => new { el.AuthorId, el.BookId });
+		}
+
 		public DbSet<Author> Author { get; set; }
 		public DbSet<Book> Book { get; set; }
 		public DbSet<Comment> Comment { get; set; }
+		public DbSet<AuthorBook> AuthorBook { get; set; }
 	}
 }

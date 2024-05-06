545aa643d719b027728a1317453d1ee2252c2922
Wesdell <wesdell17@gmail.com>
2024-05-06 17:15:22 -0500

    Add AuthorBook entity.

---
+++ library_api/Entities/AuthorBook.cs
@@ -0,0 +1,11 @@
+﻿namespace library_api.Entities
+{
+	public class AuthorBook
+	{
+		public int AuthorId { get; set; }
+		public int BookId { get; set; }
+		public int Order { get; set; }
+		public Author Author { get; set; }
+		public Book Book { get; set; }
+	}
+}

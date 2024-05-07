2abde4c87564223f6c0975990c8fce31165d80d7
Wesdell <wesdell17@gmail.com>
2024-05-07 16:59:17 -0500

    Modify mapper to generate authors ids.

--- library_api/Utils/AutoMapperProfiles.cs
+++ library_api/Utils/AutoMapperProfiles.cs
@@ -10,10 +10,24 @@ namespace library_api.Utils
 		{
 			CreateMap<CreateAuthorDTO, Author>();
 			CreateMap<Author, AuthorDTO>();
-			CreateMap<CreateBookDTO, Book>();
+			CreateMap<CreateBookDTO, Book>().ForMember(book => book.AuthorBooks, options => options.MapFrom(this.MapAuthorBook));
 			CreateMap<Book, BookDTO>();
 			CreateMap<CreateCommentDTO, Comment>();
 			CreateMap<Comment, CommentDTO>();
+		}
+
+		private List<AuthorBook> MapAuthorBook(CreateBookDTO createBookDTO, Book book)
+		{
+			List<AuthorBook> authorBooks = new List<AuthorBook>();
+			if (createBookDTO.AuthorsIds == null)
+			{
+				return authorBooks;
+			}
+			foreach (int id in createBookDTO.AuthorsIds)
+			{
+				authorBooks.Add(new AuthorBook() { AuthorId = id });
+			}
+			return authorBooks;
 		}
 	}
 }

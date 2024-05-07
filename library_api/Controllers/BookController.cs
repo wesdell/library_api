39fe538718514627ccc0d27735ea8abd35faccca
Wesdell <wesdell17@gmail.com>
2024-05-07 17:00:10 -0500

    Modify Book post endpoint to assign properly a book for each author.

--- library_api/Controllers/BookController.cs
+++ library_api/Controllers/BookController.cs
@@ -34,12 +34,27 @@ namespace library_api.Controllers
 		[HttpPost]
 		public async Task<ActionResult> Post([FromBody] CreateBookDTO createBookDTO)
 		{
-			//bool authorExists = await this._context.Author.AnyAsync(author => author.Id == book.AuthorId);
-			//if (!authorExists)
-			//{
-			//	return BadRequest($"Author: {book.AuthorId}, does not exist.");
-			//}
+			if (createBookDTO.AuthorsIds == null)
+			{
+				return BadRequest("Cannot create a book without at least one author.");
+			}
+
+			List<int> authorsIds = await this._context.Author.Where(author => createBookDTO.AuthorsIds.Contains(author.Id)).Select(author => author.Id).ToListAsync();
+			if (createBookDTO.AuthorsIds.Count != authorsIds.Count)
+			{
+				return BadRequest("There is no one of the submitted authors.");
+			}
+
 			Book book = this._mapper.Map<Book>(createBookDTO);
+
+			if (book.AuthorBooks != null)
+			{
+				for (int i = 0; i < book.AuthorBooks.Count; i += 1)
+				{
+					book.AuthorBooks[i].Order = i;
+				}
+			}
+
 			this._context.Add(book);
 			await this._context.SaveChangesAsync();
 			return Ok();

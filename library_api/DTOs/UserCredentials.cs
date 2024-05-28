7d3aef05df47ba3084724ff3880db646f9785a8c
Wesdell <wesdell17@gmail.com>
2024-05-27 21:12:53 -0500

    New DTO to control User credentials data.

---
+++ library_api/DTOs/UserCredentials.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace library_api.DTOs
+{
+	public class UserCredentials
+	{
+		[Required]
+		public string Name { get; set; }
+		[Required]
+		[EmailAddress]
+		public string Email { get; set; }
+		[Required]
+		public string Password { get; set; }
+	}
+}

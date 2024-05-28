de7e69487854afce505ad22579c2779e21970346
Wesdell <wesdell17@gmail.com>
2024-05-27 21:14:25 -0500

    Add endpoint to signup into the api

---
+++ library_api/Controllers/AccountController.cs
@@ -0,0 +1,52 @@
+﻿using library_api.DTOs;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace library_api.Controllers
+{
+	[ApiController]
+	[Route("api/account")]
+	public class AccountController : ControllerBase
+	{
+		private UserManager<IdentityUser> _userManager;
+		private IConfiguration _configuration;
+
+		public AccountController(UserManager<IdentityUser> userManager, IConfiguration configuration)
+		{
+			this._userManager = userManager;
+			this._configuration = configuration;
+		}
+
+		[HttpPost("signup")]
+		public async Task<ActionResult<AuthenticationResponse>> SignUp(UserCredentials userCredentials)
+		{
+			IdentityUser user = new IdentityUser() { UserName = userCredentials.Name, Email = userCredentials.Email };
+			IdentityResult account = await this._userManager.CreateAsync(user, userCredentials.Password);
+
+			if (!account.Succeeded)
+			{
+				return BadRequest(account.Errors);
+			}
+
+			return this.SetUserToken(userCredentials);
+		}
+
+		private AuthenticationResponse SetUserToken(UserCredentials userCredentials)
+		{
+			List<Claim> claims = new List<Claim>() {
+				new Claim("name", userCredentials.Name)
+			};
+
+			SymmetricSecurityKey key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(this._configuration["JWT_SECRET"]));
+			SigningCredentials credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+			DateTime expires = DateTime.Now.AddMonths(1);
+			JwtSecurityToken securityToken = new JwtSecurityToken(issuer: null, audience: null, claims, expires, signingCredentials: credentials);
+
+			return new AuthenticationResponse() { Token = new JwtSecurityTokenHandler().WriteToken(securityToken) };
+		}
+	}
+}

7c0d2615171130da3fc6f9fecba656c674eb33af
Wesdell <wesdell17@gmail.com>
2024-05-28 17:49:03 -0500

    New field for AuthenticationResponse DTO

--- library_api/DTOs/AuthenticationResponse.cs
+++ library_api/DTOs/AuthenticationResponse.cs
@@ -1,7 +1,9 @@
-﻿namespace library_api.DTOs
+﻿
+namespace library_api.DTOs
 {
 	public class AuthenticationResponse
 	{
 		public string Token { get; set; }
+		public DateTime Expires { get; set; }
 	}
 }

fc312d8cd6fea7ca83291b143b0c0def71cf4545
Wesdell <wesdell17@gmail.com>
2024-06-02 15:50:00 -0500

    New DTO for track HATEOAS

---
+++ library_api/DTOs/HATEOASData.cs
@@ -0,0 +1,16 @@
+﻿namespace library_api.DTOs
+{
+	public class HATEOASData
+	{
+		public string Link { get; private set; }
+		public string Description { get; private set; }
+		public string Method { get; private set; }
+
+		public HATEOASData(string link, string description, string method)
+		{
+			this.Link = link;
+			this.Description = description;
+			this.Method = method;
+		}
+	}
+}

dc9826a74f38c0faf5112e3094e140346508c84a
Wesdell <wesdell17@gmail.com>
2024-06-02 15:50:38 -0500

    Add endpoint to get HATEOAS

---
+++ library_api/Controllers/RootController.cs
@@ -0,0 +1,18 @@
+﻿using library_api.DTOs;
+using Microsoft.AspNetCore.Mvc;
+
+namespace library_api.Controllers
+{
+	[ApiController]
+	[Route("api")]
+	public class RootController : ControllerBase
+	{
+		[HttpGet(Name = "GetRoot")]
+		public ActionResult<IEnumerable<HATEOASData>> Get()
+		{
+			List<HATEOASData> hateoasData = new List<HATEOASData>();
+			hateoasData.Add(new HATEOASData(link: Url.Link("GetRoot", new { }), description: "self", method: "GET"));
+			return hateoasData;
+		}
+	}
+}

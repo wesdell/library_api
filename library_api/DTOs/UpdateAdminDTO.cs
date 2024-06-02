8c69c1457d64b8827c4677c5fea32ea1b5b1468b
Wesdell <wesdell17@gmail.com>
2024-06-01 20:05:43 -0500

    Add admin DTO

---
+++ library_api/DTOs/UpdateAdminDTO.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace library_api.DTOs
+{
+	public class UpdateAdminDTO
+	{
+		[Required]
+		[EmailAddress]
+		public string Email { get; set; }
+	}
+}

a4a4977e78504d423079f31d7e8407a7392ba128
Wesdell <wesdell17@gmail.com>
2024-06-01 19:33:45 -0500

    Remove unnecessary field

--- library_api/DTOs/UserCredentials.cs
+++ library_api/DTOs/UserCredentials.cs
@@ -5,8 +5,6 @@ namespace library_api.DTOs
 	public class UserCredentials
 	{
 		[Required]
-		public string Name { get; set; }
-		[Required]
 		[EmailAddress]
 		public string Email { get; set; }
 		[Required]

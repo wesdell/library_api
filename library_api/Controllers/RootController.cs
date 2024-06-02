0c3b1b6a330f1f77edb5167e0724c7d7f768b4d5
Wesdell <wesdell17@gmail.com>
2024-06-02 16:28:58 -0500

    Fill HATEOAS for anonymous and admin consumers

--- library_api/Controllers/RootController.cs
+++ library_api/Controllers/RootController.cs
@@ -1,17 +1,38 @@
 ﻿using library_api.DTOs;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace library_api.Controllers
 {
 	[ApiController]
 	[Route("api")]
+	[Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
 	public class RootController : ControllerBase
 	{
-		[HttpGet(Name = "GetRoot")]
-		public ActionResult<IEnumerable<HATEOASData>> Get()
+		private readonly IAuthorizationService _authorizationService;
+
+		public RootController(IAuthorizationService authorizationService)
 		{
-			List<HATEOASData> hateoasData = new List<HATEOASData>();
-			hateoasData.Add(new HATEOASData(link: Url.Link("GetRoot", new { }), description: "self", method: "GET"));
+			this._authorizationService = authorizationService;
+		}
+
+		[HttpGet(Name = "GetRoot")]
+		[AllowAnonymous]
+		public async Task<ActionResult<IEnumerable<HATEOASData>>> Get()
+		{
+			AuthorizationResult isAdmin = await this._authorizationService.AuthorizeAsync(User, "Admin");
+
+			List<HATEOASData> hateoasData = new List<HATEOASData>() {
+				new HATEOASData(link: Url.Link("GetRoot", new {}), description: "self", method: "GET"),
+				new HATEOASData(link: Url.Link("GetAuthors", new {}), description: "authors", method: "GET")
+			};
+
+			if (isAdmin.Succeeded)
+			{
+				hateoasData.Add(new HATEOASData(link: "CreateAuthor", description: "create-author", method: "POST"));
+			}
+
 			return hateoasData;
 		}
 	}

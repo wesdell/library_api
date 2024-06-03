c1cf490e5085790f81b5ed42790f7566c0d560ac
Wesdell <wesdell17@gmail.com>
2024-06-03 18:30:22 -0500

    Extract HATEOAS links generation to a filter

---
+++ library_api/Utils/HATEOASAuthorFilterAttribute.cs
@@ -0,0 +1,42 @@
+﻿using library_api.DTOs;
+using library_api.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace library_api.Utils
+{
+	public class HATEOASAuthorFilterAttribute : HATEOASFilterAttribute
+	{
+		private readonly SetHATEOASLinks _setHATEOASLinks;
+
+		public HATEOASAuthorFilterAttribute(SetHATEOASLinks setHATEOASLinks)
+		{
+			this._setHATEOASLinks = setHATEOASLinks;
+		}
+
+		public override async Task OnResultExecutionAsync(ResultExecutingContext context, ResultExecutionDelegate next)
+		{
+			bool includeHATEOAS = this.InlcudeHATEOAS(context);
+			if (!includeHATEOAS)
+			{
+				await next();
+				return;
+			}
+
+			ObjectResult objectResult = context.Result as ObjectResult;
+			AuthorDTO authorDTO = objectResult.Value as AuthorDTO;
+			if (authorDTO == null)
+			{
+				List<AuthorDTO> authorDTOs = objectResult.Value as List<AuthorDTO> ?? throw new ArgumentNullException("An AuhtorDTO instance was expected.");
+				authorDTOs.ForEach(async author => await this._setHATEOASLinks.Set(author));
+				objectResult.Value = authorDTOs;
+			}
+			else
+			{
+				await this._setHATEOASLinks.Set(authorDTO);
+			}
+
+			await next();
+		}
+	}
+}
---
+++ library_api/Utils/HATEOASFilterAttribute.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Primitives;
+
+namespace library_api.Utils
+{
+	public class HATEOASFilterAttribute : ResultFilterAttribute
+	{
+		protected bool InlcudeHATEOAS(ResultExecutingContext context)
+		{
+			ObjectResult result = context.Result as ObjectResult;
+			if (!this.IsSuccessfulResponse(result))
+			{
+				return false;
+			}
+
+			StringValues header = context.HttpContext.Request.Headers["includeHATEOAS"];
+			if (header.Count == 0)
+			{
+				return false;
+			}
+
+			string headerValue = header[0];
+			if (!headerValue.Equals("yes", StringComparison.InvariantCultureIgnoreCase))
+			{
+				return false;
+			}
+
+			return true;
+		}
+
+		private bool IsSuccessfulResponse(ObjectResult objectResult)
+		{
+			if (objectResult == null || objectResult.Value == null)
+			{
+				return false;
+			}
+			if (objectResult.StatusCode.HasValue && !objectResult.StatusCode.Value.ToString().StartsWith("2"))
+			{
+				return false;
+			}
+			return true;
+		}
+	}
+}

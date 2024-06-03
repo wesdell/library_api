ea6203a80b0c2bb5fb12a58f3bc73ded5c5c2ca5
Wesdell <wesdell17@gmail.com>
2024-06-03 18:42:25 -0500

    Set up swagger config to not required HATEOAS parameter on endpoint

---
+++ library_api/Utils/HATEOASParameter.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace library_api.Utils
+{
+	public class HATEOASParameter : IOperationFilter
+	{
+		public void Apply(OpenApiOperation operation, OperationFilterContext context)
+		{
+			if (context.ApiDescription.HttpMethod != HttpMethod.Get.ToString())
+			{
+				return;
+			}
+
+			if (operation.Parameters == null)
+			{
+				operation.Parameters = new List<OpenApiParameter>();
+			}
+
+			operation.Parameters.Add(
+				new OpenApiParameter
+				{
+					Name = "includeHATEOAS",
+					In = ParameterLocation.Header,
+					Required = false
+				}
+			);
+		}
+	}
+}

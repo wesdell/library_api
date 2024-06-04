73783bb76317601f331550fb1f07a38877976b17
Wesdell <wesdell17@gmail.com>
2024-06-04 18:34:09 -0500

    Set up IQueryable method to paginate

---
+++ library_api/Utils/IQueryableExtensions.cs
@@ -0,0 +1,12 @@
+﻿using library_api.DTOs;
+
+namespace library_api.Utils
+{
+	public static class IQueryableExtensions
+	{
+		public static IQueryable<T> Paginate<T>(this IQueryable<T> queryable, PaginationDTO paginationDTO)
+		{
+			return queryable.Skip((paginationDTO.Page - 1) * paginationDTO.RecordsPerPage).Take(paginationDTO.RecordsPerPage);
+		}
+	}
+}

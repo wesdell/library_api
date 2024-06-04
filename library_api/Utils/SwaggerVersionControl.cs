a431762fc8389b9af0209e3a291417fefb7868f3
Wesdell <wesdell17@gmail.com>
2024-06-04 17:28:22 -0500

    New util to manage api version control

---
+++ library_api/Utils/SwaggerVersionControl.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc.ApplicationModels;
+
+namespace library_api.Utils
+{
+	public class SwaggerVersionControl : IControllerModelConvention
+	{
+		public void Apply(ControllerModel controller)
+		{
+			string controllerNamespace = controller.ControllerType.Namespace;
+			string apiVersion = controllerNamespace.Split('.').Last().ToLower();
+			controller.ApiExplorer.GroupName = apiVersion;
+		}
+	}
+}

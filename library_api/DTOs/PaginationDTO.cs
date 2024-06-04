40e9c3b8f4474cc3ef8692dcf698c6e6afc33e1b
Wesdell <wesdell17@gmail.com>
2024-06-04 18:34:51 -0500

    Add PaginationDTO data

---
+++ library_api/DTOs/PaginationDTO.cs
@@ -0,0 +1,22 @@
+﻿namespace library_api.DTOs
+{
+	public class PaginationDTO
+	{
+		public int Page { get; set; } = 1;
+		private int RecordsToShowPerPage = 10;
+		private readonly int MaximumRecordsPerPage = 20;
+
+		public int RecordsPerPage
+		{
+			get
+			{
+				return this.RecordsToShowPerPage;
+			}
+
+			set
+			{
+				this.RecordsToShowPerPage = (value > MaximumRecordsPerPage) ? this.MaximumRecordsPerPage : value;
+			}
+		}
+	}
+}

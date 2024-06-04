1911c7627723e927977c4952d8f133176279d9f8
Wesdell <wesdell17@gmail.com>
2024-06-04 18:33:40 -0500

    Include a method on HttpContext to set pagination header

---
+++ library_api/Utils/HttpContextExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace library_api.Utils
+{
+	public static class HttpContextExtensions
+	{
+		public async static Task InsertPaginationParametersInHeader<T>(this HttpContext httpContext, IQueryable<T> queryable)
+		{
+			ArgumentNullException.ThrowIfNull(httpContext);
+
+			double amount = await queryable.CountAsync();
+			httpContext.Response.Headers.Append("recordsAmount", amount.ToString());
+		}
+	}
+}
